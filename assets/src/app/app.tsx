42948baaf50519c0d5accc0fd678c02f508192b4
Lucas Medeiros <lucas.medeirosleite@shopify.com>
2018-11-11 19:00:16 -0500

    Add polaris

--- assets/src/app/app.tsx
+++ assets/src/app/app.tsx
@@ -1,3 +1,4 @@
+import { AppProvider, Card, Page } from "@shopify/polaris";
 import * as React from "react";
 import * as Styles from "./app.scss";
 import { Header, Logo } from "./shared/components";
@@ -5,12 +6,18 @@ import { Header, Logo } from "./shared/components";
 export class App extends React.PureComponent<{}, {}> {
   public render() {
     return (
-      <div className={Styles.MainContainer}>
-        <div className={Styles.Topbar}>
-          <Logo />
-          <Header />
-        </div>
-      </div>
+      <AppProvider>
+        <Page title="Taskify">
+          <Card sectioned={true}>
+            <div className={Styles.MainContainer}>
+              <div className={Styles.Topbar}>
+                <Logo />
+                <Header />
+              </div>
+            </div>
+          </Card>
+        </Page>
+      </AppProvider>
     );
   }
 }
